eb9a7a4de0ca18ef6282c9bf0025a06fee968411
kjeschke9 <kjesc@msn.com>
2026-07-08 02:39:38 +0000

    Create HiddenContentManagerScreen as default export

---
+++ src/screens/HiddenContentManagerScreen.tsx
@@ -0,0 +1,5 @@
+// src/screens/HiddenContentManagerScreen.tsx
+// AppNavigator imports HiddenContentManagerScreen as a default export.
+// HiddenTitlesScreen is a named export  this file adapts it.
+import { HiddenTitlesScreen } from './HiddenTitlesScreen';
+export default HiddenTitlesScreen;
