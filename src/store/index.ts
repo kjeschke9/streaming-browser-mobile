8b2b990a429ee75e495fa22f531e3a6979ca4fea
kjeschke9 <kjesc@msn.com>
2026-07-10 00:16:40 -0700

    Refactor store configuration in index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -20,7 +20,7 @@ export const store = configureStore({
           }),
 });
 
-export type RootState   = ReturnType>typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 // Re-export Zustand stores so hooks can import from '../store'
